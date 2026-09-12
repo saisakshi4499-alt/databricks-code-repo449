9e8059b573409eec74a1c0b21f1cc611c2e2992f
saisakshi4499-alt <sai.sakshi4499@gmail.com>
2026-09-12 09:29:49 +0530

    Task1 for Week1 Sep

---
+++ Python/Task1.py
@@ -0,0 +1,74 @@
+teams = ['Data', 'AI', 'DevOps']
+for t in teams:
+ print('Hello', t, 'Team from Inceptez Technologies')
+print('Keep Learning and Exploring!')
+print("\n")
+
+
+# Initializing and Assigning Variables
+students = 100
+trainers = 2
+# Adding sum of students and trainers
+total = students + trainers
+print(total)
+
+
+'''print("Welcome to Inceptez Python Learning")'''
+
+a='''print("Welcome to Inceptez Python Learning")'''
+print(a)
+
+b = '''This is Inceptez's "Python" class for Data Engineers & AI Engineers'''
+print(b)
+
+
+c = '''Welcome to Inceptez Technologies!
+Python Training: Basics
+Enjoy your learning journey.'''
+
+print(c)
+
+
+Student = 'Sai'
+Course = 'Data Engineer'
+Institue = 'Inceptez'
+
+print("Name:", Student ,"is learning the course", Course,"at the institute", Institue)
+
+
+fee = 450000
+gst = fee * 0.18
+total = fee + gst
+print(total)
+
+
+
+
+
+#2student = 'Ravi'
+_student_id = 1001
+studentName = 'Priya'
+#class name = 'Python'
+inceptez_batch = 'Morning'
+
+print(inceptez_batch)
+
+#print(2student)
+
+print(studentName)
+
+print(_student_id)
+
+#print(class name)
+
+DataEngineeringBatch = 'Sai'
+
+dataEngineeringBatch = 'Sai'
+
+data_engineering_batch = 648_223_568
+
+print(data_engineering_batch)
+
+print(dataEngineeringBatch)
+
+print(DataEngineeringBatch)
